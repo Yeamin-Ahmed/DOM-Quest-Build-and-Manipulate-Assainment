5cf0f4f5d123454f52380192f241a2d9e963be3f
Yeamin Ahmed <137269221+Yeamin-Ahmed@users.noreply.github.com>
2024-09-24 21:43:05 +0600

    10%

---
+++ js/utilities.js
@@ -0,0 +1,16 @@
+// function1
+function getInputValueById(id){
+const inputValue = document.getElementById(id).Value;
+const inputNumber = parseFloat(inputValue);
+return inputNumber;
+}
+
+
+// function2
+
+
+function getTextValueById(id){
+const inputValue = document.getElementById(id).Value;
+const inputNumber = parseFloat(inputValue);
+return inputNumber;
+}
