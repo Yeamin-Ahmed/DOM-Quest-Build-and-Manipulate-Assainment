a6a68b0d59e8a8846833a2981812d677af796604
Yeamin Ahmed <137269221+Yeamin-Ahmed@users.noreply.github.com>
2024-09-25 14:52:15 +0600

    60%

--- js/add-Donation.js
+++ js/add-Donation.js
@@ -71,3 +71,81 @@ document.getElementById('history-container').appendChild(div);
 
 });
 
+
+// *******************************************
+
+
+
+document.getElementById("add-donation1")
+.addEventListener("click", function(event)
+{
+    event.preventDefault();
+
+    const addMoney = getInputValueById('input-add-money1');
+
+   
+    if (isNaN(addMoney)){
+        alert('টাকা দেন ভাই');
+        return;
+    }
+const balance = getTextValueById('donate-balance2');
+
+const newBal = balance + addMoney;
+
+document.getElementById('donate-balance2').innerText = newBal;
+
+
+});
+// *************************
+
+
+// main balance theke taka kata!
+
+document.getElementById("add-donation1").addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    const addMoney = getInputValueById('input-add-money1');
+
+    
+    if (isNaN(addMoney)) {
+        alert('টাকা দেন ভাই'); 
+        return;
+    }
+
+    const balance = getTextValueById('account-balance'); 
+
+    
+    if (addMoney > balance) {
+        alert('ভাই তুই কি নিজেরে হাতিম তাই ভাবস');
+        return;
+    }
+
+   
+    const newBalance = balance - addMoney;
+    document.getElementById('account-balance').innerText = newBalance; 
+
+    // history
+const  div = document.createElement('div');
+div.classList.add ('text-2xl','font-bold','box-border',);
+
+const historyTime = new Date();
+const formattedDateTime = historyTime.toLocaleString();
+
+div.innerHTML =`
+
+<p>${addMoney} BDT. for Flood at Feni, Bangladesh</p>
+<p class = "font-light"><strong>Date:</strong> ${formattedDateTime}</p>
+
+`
+document.getElementById('history-container').appendChild(div);
+
+// else{
+//     alert('gotten')
+// }
+
+
+
+
+
+});
+
